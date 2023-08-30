e5e58984bc1e173ebd8a82da7bf8a8bc76d10d6d
jakubgrad <jakub.grad@icloud.com>
2023-08-30 14:11:22 +0200

    Finished part 5 without exercise 5.15

--- part5/bloglist end to end testing/5.23/cypress/e2e/blog_app.cy.js
+++ part5/bloglist end to end testing/5.23/cypress/e2e/blog_app.cy.js
@@ -5,7 +5,7 @@ describe('Blog app', function() {
       cy.request('POST', 'http://localhost:3003/api/testing/reset')
       cy.visit('http://localhost:3000')
     })
-  
+
     it('Login form is shown', function() {
       cy.visit('http://localhost:3000')
       cy.contains('Blogs')
@@ -20,7 +20,7 @@ describe('Blog app', function() {
         username: 'kuba',
         password: 'kuba'
       }
-      cy.request('POST', 'http://localhost:3003/api/users/', user) 
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
       cy.visit('http://localhost:3000')
       cy.contains('login').click()
       cy.get('input:first').type('kuba')
@@ -35,14 +35,14 @@ describe('Blog app', function() {
         username: 'kuba',
         password: 'kuba'
       }
-      cy.request('POST', 'http://localhost:3003/api/users/', user) 
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
       cy.visit('http://localhost:3000')
       cy.contains('login').click()
       cy.get('input:first').type('kuba')
       cy.get('input:last').type('wrongpassword')
       cy.get('#login-button').click()
       cy.get('#login-button').click()
-      cy.get('#error').should('contain', 'wrong username or password') 
+      cy.get('#error').should('contain', 'wrong username or password')
         .and('have.css', 'color', 'rgb(255, 0, 0)')
         .and('have.css', 'border-style', 'solid')
       cy.get('html').should('not.contain', 'kuba kuba logged in')
@@ -53,7 +53,6 @@ describe('Blog app', function() {
   it('displays the login form by default', function() {
     cy.visit('http://localhost:3000')
     cy.contains('Blogs')
-    //cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
   })
 
   describe('5.19 creating a new blog', function() {
@@ -64,7 +63,7 @@ describe('Blog app', function() {
         username: 'kuba',
         password: 'kuba'
       }
-      cy.request('POST', 'http://localhost:3003/api/users/', user) 
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
       cy.visit('http://localhost:3000')
       cy.contains('login').click()
       cy.get('input:first').type('kuba')
@@ -91,7 +90,7 @@ describe('Blog app', function() {
         username: 'kuba',
         password: 'kuba'
       }
-      cy.request('POST', 'http://localhost:3003/api/users/', user) 
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
       cy.visit('http://localhost:3000')
       cy.contains('login').click()
       cy.get('input:first').type('kuba')
@@ -114,53 +113,38 @@ describe('Blog app', function() {
 
   describe('5.21 a user can delete their blog', function() {
     beforeEach(function() {
-        cy.request('POST', 'http://localhost:3003/api/testing/reset')
-        const user = {
-          name: 'kuba kuba',
-          username: 'kuba',
-          password: 'kuba'
-        }
-        cy.request('POST', 'http://localhost:3003/api/users/', user)
-        
-        const user2 = {
-          name: 'kuba2',
-          username: 'kuba2',
-          password: 'kuba'
-        }
-        cy.request('POST', 'http://localhost:3003/api/users/', user2)
-        
-        cy.login({ username: 'kuba', password: 'kuba' })
-        cy.createBlog({
-          title: 'title from cy',
-          author: 'author is kuba kuba',
-          url: 'the url url'
-        })
-        cy.createBlog({
-          title: 'casual custom createBlog command title',
-          author: 'custom author',
-          url: 'everything is custom'
-        })
-      
+      cy.request('POST', 'http://localhost:3003/api/testing/reset')
+      const user = {
+        name: 'kuba kuba',
+        username: 'kuba',
+        password: 'kuba'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
 
-      //cy.request('POST', 'http://localhost:3003/api/testing/reset')
-      //cy.visit('http://localhost:3000')
-      //cy.contains('login').click()
-      //cy.get('input:first').type('kuba')
-      //cy.get('input:last').type('kuba')
-      //cy.get('#login-button').click()
-      /*cy.contains('new blog').click()
-      cy.get('input:eq(0)').type('title from cy')
-      cy.get('input:eq(1)').type('author from cy')
-      cy.get('input:eq(2)').type('url from cy')
-      cy.contains('save').click()*/
-      
+      const user2 = {
+        name: 'kuba2',
+        username: 'kuba2',
+        password: 'kuba'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', user2)
+
+      cy.login({ username: 'kuba', password: 'kuba' })
+      cy.createBlog({
+        title: 'title from cy',
+        author: 'author is kuba kuba',
+        url: 'the url url'
+      })
+      cy.createBlog({
+        title: 'casual custom createBlog command title',
+        author: 'custom author',
+        url: 'everything is custom'
+      })
     })
 
     //5.21
     it('a blog can be deleted by the user who created it', function() {
       cy.contains('title from cy')
       cy.contains('remove').click()
-      //cy.on('window:confirm', () => true);
       cy.window().then((win) =>
         cy.stub(win, 'confirm').as('confirm').returns(true),
       )
@@ -168,15 +152,8 @@ describe('Blog app', function() {
     })
 
     //5.22
-    it('only the creater sees the reomve button for their blogs', function() {
+    it('5.22 only the creater sees the reomve button for their blogs', function() {
       cy.login({ username: 'kuba2', password: 'kuba' })
-      
-      //cy.contains('title from cy').parent().
-     // cy.contains('remove').click()
-      //cy.on('window:confirm', () => true);
-      //cy.window().then((win) =>
-      //  cy.stub(win, 'confirm').as('confirm').returns(true),
-      //)
       cy.get('.blogStyle2').should('not.contain','remove')
     })
 
@@ -184,51 +161,48 @@ describe('Blog app', function() {
 
   describe('5.23 blogs are ordered according to likes with the blog with the most likes being first.', function() {
     beforeEach(function() {
-        cy.request('POST', 'http://localhost:3003/api/testing/reset')
-        const user = {
-          name: 'kuba kuba',
-          username: 'kuba',
-          password: 'kuba'
-        }
-        cy.request('POST', 'http://localhost:3003/api/users/', user)
-        
-        cy.login({ username: 'kuba', password: 'kuba' })
-        cy.createBlog({
-          title: 'The title with the least number of likes',
-          author: 'author is kuba kuba',
-          url: 'the url url'
-        })
-        cy.createBlog({
-          title: 'The title with the most likes',
-          author: 'custom author',
-          url: 'everything is custom'
-        })
-        cy.createBlog({
-          title: 'The title with the second most likes',
-          author: 'handkerchief',
-          url: 'Oooo.-rah'
-        })
-      
+      cy.request('POST', 'http://localhost:3003/api/testing/reset')
+      const user = {
+        name: 'kuba kuba',
+        username: 'kuba',
+        password: 'kuba'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
+
+      cy.login({ username: 'kuba', password: 'kuba' })
+      cy.createBlog({
+        title: 'The title with the least number of likes',
+        author: 'author is kuba kuba',
+        url: 'the url url'
+      })
+      cy.createBlog({
+        title: 'The title with the most likes',
+        author: 'custom author',
+        url: 'everything is custom'
+      })
+      cy.createBlog({
+        title: 'The title with the second most likes',
+        author: 'handkerchief',
+        url: 'Oooo.-rah'
+      })
+
     })
 
     //5.23
     it('blogs are arranged in order', function() {
-      cy.likeBlogByTitle({ title: 'The title with the most likes'})
-      cy.likeBlogByTitle({ title: 'The title with the most likes'})
-      cy.likeBlogByTitle({ title: 'The title with the most likes'})
-      cy.likeBlogByTitle({ title: 'The title with the second most likes'})
-      cy.likeBlogByTitle({ title: 'The title with the second most likes'})
-      cy.likeBlogByTitle({ title: 'The title with the least number of likes'})
-      
+      cy.likeBlogByTitle({ title: 'The title with the most likes' })
+      cy.likeBlogByTitle({ title: 'The title with the most likes' })
+      cy.likeBlogByTitle({ title: 'The title with the most likes' })
+      cy.likeBlogByTitle({ title: 'The title with the second most likes' })
+      cy.likeBlogByTitle({ title: 'The title with the second most likes' })
+      cy.likeBlogByTitle({ title: 'The title with the least number of likes' })
+
       cy.get('.blogStyle2').eq(0).should('contain', 'The title with the most likes')
       cy.get('.blogStyle2').eq(1).should('contain', 'The title with the second most likes')
       cy.get('.blogStyle2').eq(2).should('contain', 'The title with the least number of likes')
     })
 
   })
-
-  
-  
 
 
 })
--- part5/bloglist end to end testing/5.23/cypress/support/commands.js
+++ part5/bloglist end to end testing/5.23/cypress/support/commands.js
@@ -24,31 +24,31 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 Cypress.Commands.add('login', ({ username, password }) => {
-    cy.request('POST', 'http://localhost:3003/api/login', {
-      username, password
-    }).then(({ body }) => {
-      localStorage.setItem('loggedBlogappUser', JSON.stringify(body))
-      cy.visit('') //cy.visit('http://localhost:3000')
-    })
+  cy.request('POST', 'http://localhost:3003/api/login', {
+    username, password
+  }).then(({ body }) => {
+    localStorage.setItem('loggedBlogappUser', JSON.stringify(body))
+    cy.visit('') //cy.visit('http://localhost:3000')
   })
+})
 
 Cypress.Commands.add('createBlog', ({ title, author, url }) => {
-cy.request({
+  cy.request({
     url: 'http://localhost:3003/api/blogs',
     method: 'POST',
     body: { title, author, url },
     headers: {
-    'Authorization': `Bearer ${JSON.parse(localStorage.getItem('loggedBlogappUser')).token}`
+      'Authorization': `Bearer ${JSON.parse(localStorage.getItem('loggedBlogappUser')).token}`
     }
-})
+  })
 
-cy.visit('') //cy.visit('http://localhost:3000')
+  cy.visit('') //cy.visit('http://localhost:3000')
 })
 
 Cypress.Commands.add('likeBlogByTitle', ({ title }) => {
-    cy.visit('') //cy.visit('http://localhost:3000')
-    cy.contains('.blogStyle2', title)
-        .find('.likeButton')
-        .click()
-      cy.get('html').should('contain',`Successfully liked ${title}`)
-    })
+  cy.visit('') //cy.visit('http://localhost:3000')
+  cy.contains('.blogStyle2', title)
+    .find('.likeButton')
+    .click()
+  cy.get('html').should('contain',`Successfully liked ${title}`)
+})
